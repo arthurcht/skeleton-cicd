b80c03d7e1e3da4b94d9a64eaf015068e0c5824f
arthurcht <arthur@dataroots.io>
2022-07-13 17:51:58 +0200

    confirm 'QA' and 'TEST' jobs in cicd.yml

--- to_lint.py
+++ to_lint.py
@@ -1,5 +1,15 @@
+from datetime import date
+from os import path
+import time
 import os
-import sys
-from datetime import date, time
+from datetime import datetime
 
-black_this = [1, 2, 3]
+black_this = [1,
+        2,
+              3]
+path = './README.md'
+exist =    os.path.exists(path)
+print(exist)
+print(time.time())
+print(date.today())
+print(datetime.today())
