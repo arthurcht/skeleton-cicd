e29237276f0afae3e2cb4e4444050cf35a1bee8b
arthurcht <arthurcht@users.noreply.github.com>
2022-07-13 15:52:23 +0000

    Commit black- and isort- reformatted code

--- to_lint.py
+++ to_lint.py
@@ -1,14 +1,11 @@
-from datetime import date
-from os import path
-import time
 import os
-from datetime import datetime
+import time
+from datetime import date, datetime
+from os import path
 
-black_this = [1,
-        2,
-              3]
-path = './README.md'
-exist =    os.path.exists(path)
+black_this = [1, 2, 3]
+path = "./README.md"
+exist = os.path.exists(path)
 print(exist)
 print(time.time())
 print(date.today())
